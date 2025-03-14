42af0d765df9367457577c59c08b0bcfd6c27553
eduardasiq-moura <eduardasiq.moura@gmail.com>
2025-03-14 10:46:12 -0300

    Att

--- Curso_JavaScript/Dia_7/index.js
+++ Curso_JavaScript/Dia_7/index.js
@@ -21,26 +21,26 @@ function compareNumbers() {
 
     if (attempts < maxguesses) {
         if (userNumber > computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('textOutput').innerHTML = 'Seu número é muito alto'
             document.getElementById('inputBox').value = ''
             attempts++
             document.getElementById('attempts').innerHTML = attempts
            }
            else if (userNumber < computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too low'
+            document.getElementById('textOutput').innerHTML = 'Seu número é muito baixo'
             document.getElementById('inputBox').value = ''
             attempts++
             document.getElementById('attempts').innerHTML = attempts
            }
            else {
-            document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+            document.getElementById('textOutput').innerHTML = 'Parabéns!!!'
             attempts++
             document.getElementById('attempts').innerHTML = attempts
             document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
            }
     }
     else {
-        document.getElementById('textOutput').innerHTML = 'You Lose! The computer number was ' + computerNumber
+        document.getElementById('textOutput').innerHTML = 'Você perdeu! O número do computador era ' + computerNumber
         document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
     }
 
